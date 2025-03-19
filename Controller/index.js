18cf706f9fa495a762bb8c62df530a167a2a530f
Tapioka-788 <guilhermezuchellifelipetto@gmail.com>
2025-03-19 15:20:57 -0300

    correcoes

--- Controller/index.js
+++ Controller/index.js
@@ -3,17 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const valor2 = document.getElementById("valor2");
     const botoes = document.querySelectorAll(".funcBtn");
     const botaoIgual = document.getElementById("igual");
-    const botaoLimpar = document.getElementById("limpar"); // Botão de limpar
+    const botaoLimpar = document.getElementById("limpar");
     let operador = null;
 
-    // Capturar o operador selecionado
     botoes.forEach((botao) => {
         botao.addEventListener("click", function () {
-            operador = this.textContent; // Pega o símbolo do botão clicado
+            operador = this.textContent;
         });
     });
 
-    // Calcular o resultado quando clicar no botão "Igual"
     botaoIgual.addEventListener("click", function () {
         const num1 = parseFloat(valor1.value);
         const num2 = parseFloat(valor2.value);
@@ -23,7 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
             alert("Por favor, insira números válidos.");
             return;
         }
-
         switch (operador) {
             case "+":
                 resultado = num1 + num2;
@@ -41,8 +38,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("Selecione uma operação.");
                 return;
         }
-
-        // Exibir o resultado na tela
         let resultadoElemento = document.getElementById("resultado");
         if (!resultadoElemento) {
             resultadoElemento = document.createElement("h2");
@@ -51,8 +46,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         resultadoElemento.textContent = `Resultado: ${resultado}`;
     });
-
-    // Limpar os campos e o resultado
     botaoLimpar.addEventListener("click", function () {
         valor1.value = "";
         valor2.value = "";
